2bc82e790aa361fc04008f80bc6b7afff7e2412d
Paweł Dobrowolski <pawwel3@gmail.com>
2020-06-15 08:34:05 +0200

    Updates

---
+++ precache-manifest.47646c58987277515cecd2d4d54aed67.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e6766a914872456ad0cb7bc0ff38b5",
+    "url": "/ping-pong/index.html"
+  },
+  {
+    "revision": "29b37ed2a81114638f76",
+    "url": "/ping-pong/static/css/main.0fb77471.chunk.css"
+  },
+  {
+    "revision": "85a563178ad20b6c0292",
+    "url": "/ping-pong/static/js/2.18e904fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ping-pong/static/js/2.18e904fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29b37ed2a81114638f76",
+    "url": "/ping-pong/static/js/main.1464404d.chunk.js"
+  },
+  {
+    "revision": "b44d370731109b9a38d9",
+    "url": "/ping-pong/static/js/runtime-main.a8f26b41.js"
+  }
+]);
